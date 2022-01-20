54ed6bba7525fff30411046d7fa92f55f438e233
Luc4sguilherme <lucas.guilherme.matutina@gmail.com>
2022-01-20 16:39:00 -0300

    first commit

---
+++ loop.js
@@ -0,0 +1,25 @@
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomNumbers(quantity, min = 0, max = quantity) {
+  const numbers = [];
+
+  const range = min === 0 ? max - min + 1 : max - min;
+
+  if (quantity > range) {
+    throw new Error("Invalid quantity. The quantity must be less than the range between the minimum and maximum.");
+  }
+
+  while (numbers.length < quantity) {
+    const number = getRandomInt(min, max);
+
+    if (!numbers.includes(number)) {
+      numbers.push(number);
+    }
+  }
+
+  return numbers;
+}
